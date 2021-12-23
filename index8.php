baf7442d014f791c54d99a9f94e8f8777124de82
HappyVasia <57608587+HappyVasia@users.noreply.github.com>
2021-12-23 23:58:14 +0300

    Update index8.php

--- index8.php
+++ index8.php
@@ -1,25 +1,18 @@
 <?php
-
-//9.Дано натуральное число n.
-//        Вычислить: 11 + 22 + .. + nn.
- //       Вывести на экран квадраты этих чисел,
-//        а также сумму квадратов этих чисел.
+// 9.Дано натуральное число n.
+//   Вычислить: 11 + 22 + .. + nn.
+//   Вывести на экран квадраты этих чисел,
+//   а также сумму квадратов этих чисел.
 
 $int = 5;
 $int_str = (string)($int);
-if(is_string($int_str))    {echo "string";}
 $new_string = $int_str.$int_str;
-echo $new_string;
 $new_int = (integer)($new_string);
-echo $new_int;echo "<br>";
 
-$summa = $new_int;
-while($new_int != 11){
-    do {
-        $a = $new_int - 11;
-        echo $a; echo "<br>";
-        $summa += $a;
-        echo $summa; echo "<br>";
-    }
-}    
-
+	for ($new_int; $new_int !== 0; ($new_int -= 11)) {
+		$summa[] = pow($new_int, 2);
+	}
+	foreach ($summa as $value) {
+		echo $value.'<br/>';
+	}
+	echo array_sum($summa);
