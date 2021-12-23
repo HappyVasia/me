64d1475030b180680e9cdaa41cfb61edee8c6d18
HappyVasia <57608587+HappyVasia@users.noreply.github.com>
2021-12-24 01:12:31 +0300

    Update index5.php

--- index5.php
+++ index5.php
@@ -1,21 +1,24 @@
 <?php
 
-//6. Вашим заданием будет создание сокращенного
-//варианта ФИО.
-//Например, вводим: Иванов Иван Петрович,
-//        а в результате должны получить: Иванов И. П.
+// 6. Вашим заданием будет создание сокращенного
+// варианта ФИО.
+// Например, вводим: Иванов Иван Петрович,
+// а в результате должны получить: Иванов И. П.
 
 $text = $_POST["text"];
-echo $text;
-echo "<br>";
+$array = [];
+$word  =  strtok($text, " ");
+	while($word) {
+		$array[] = $word;
+		$word = strtok(" ");
+	}
+	
+$new_array = array("a" => $array[0], "b" => $array[1],
+	"c" => $array[2]);
+extract($new_array);
+//echo $a. " " . $b. " " .$c;
 
-$array = explode(",", $text);
+$new_b = trim($b);
+$new_c = trim($c);
 
-foreach ($array as $value) {
-    echo $value; echo "<br>";
-    
-}
-$count = count($array);
-echo $count;
-$nulll = $text[5];
-echo $nulll;
+printf("%s %.2s.\t %.2s.", $a,$b,$c);
